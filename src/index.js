a8821f159a4bc8222c0a295881a79044fd4cd395
Lucas Minervini <Lucas.Minervini@outlook.com>
2024-06-20 11:17:32 -0300

    Modify index

---
+++ src/index.js
@@ -0,0 +1,17 @@
+ÿþconst express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Middleware para servir archivos estÃ¡ticos desde el directorio 'public'
+app.use(express.static('public'));
+
+// Ruta para servir inicio.html como pÃ¡gina principal (index.html)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/inicio.html'));
+});
+
+// Iniciar el servidor
+app.listen(port, (=> {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
